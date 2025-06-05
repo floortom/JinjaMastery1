62ee58b9d2bbaf62c1ae4377f5752658cd5e4171
PLFLORCZUKT <Tomasz.Florczuk@tetrapak.com>
2025-06-05 10:29:26 +0200

    Added Jinja inheritance

--- app.py
+++ app.py
@@ -12,8 +12,14 @@ def create_app():
     def home():
         return render_template("home.html", todos=todos)
 
-    @app.route("/todos")
-    def to_dos():
-        return render_template("todos.html", todos=todos)
+    @app.route("/<string:todo>")
+    def todo_item(todo: str):
+        for text, completed in todos:
+            if text == todo:
+                completedText = "[x]" if completed else "[]"
+                title = f"{completedText} - Todos"
+                return render_template("todo.html", title=title, text=text, completed=completed)
+        else:
+            return render_template("notFound.html", text=todo, title="Not found")
 
     return app
